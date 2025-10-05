fa4b3dca2d0d9a2bdb0e20f002d55c390a5e3dae
marcellopz <marcellopz@hotmail.com>
2025-10-04 21:27:48 -0300

    player-page fix

--- app/player/[playerId]/page.tsx
+++ app/player/[playerId]/page.tsx
@@ -3,9 +3,10 @@ import { getPlayer } from "@/lib/endpoints";
 export default async function PlayerPage({
   params,
 }: {
-  params: { playerId: string };
+  params: Promise<{ playerId: string }>;
 }) {
-  const player = await getPlayer(params.playerId);
+  const { playerId } = await params;
+  const player = await getPlayer(playerId);
   console.log(player);
   return (
     <div className="container mx-auto px-4 py-8">
@@ -13,7 +14,7 @@ export default async function PlayerPage({
         <div>
           <h1 className="text-2xl font-bold tracking-tight">PlayerPage</h1>
           <p className="text-sm text-muted-foreground">
-            PlayerPage {params.playerId} {player?.name}
+            PlayerPage {playerId} {player?.name}
           </p>
         </div>
       </div>
