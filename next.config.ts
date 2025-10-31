3ee657dc9a4054e661361bc7ac4d274fcb838f3c
marcellopz <marcellopz@hotmail.com>
2025-10-30 22:50:49 -0300

    css fix

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,12 @@
 import type { NextConfig } from "next";
+import { fileURLToPath } from "url";
+import { dirname, resolve } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const nextConfig: NextConfig = {
+  outputFileTracingRoot: resolve(__dirname),
   images: {
     unoptimized: true,
     remotePatterns: [
