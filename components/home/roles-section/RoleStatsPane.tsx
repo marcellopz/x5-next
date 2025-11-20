ad0bf4a7e28515721f43b528d98b565771599bcd
marcellopz <marcellopz@hotmail.com>
2025-11-20 20:56:52 -0300

    typing

--- components/home/roles-section/RoleStatsPane.tsx
+++ components/home/roles-section/RoleStatsPane.tsx
@@ -1,7 +1,11 @@
 import React, { useMemo } from "react";
 import MiniStatCard from "./MiniStatCard";
 import { ROLE_META, RoleKey, RoleStat, TITLE_TO_API_MAPPING } from "./roleMeta";
-import { RoleLeaderboardData, RoleLeaderboardStats } from "@/lib/types";
+import {
+  LeaderboardEntry,
+  RoleLeaderboardData,
+  RoleLeaderboardStats,
+} from "@/lib/types";
 
 interface RoleStatsPaneProps {
   activeRole: RoleKey;
@@ -23,7 +27,9 @@ export function RoleStatsPane({
       const mapping = TITLE_TO_API_MAPPING[stat.title];
 
       if (mapping && roleData?.[mapping.apiKey as keyof RoleLeaderboardStats]) {
-        const entry = roleData[mapping.apiKey as keyof RoleLeaderboardStats];
+        const entry = roleData[
+          mapping.apiKey as keyof RoleLeaderboardStats
+        ] as LeaderboardEntry;
         return {
           title: stat.title,
           value: entry.legend_name,
